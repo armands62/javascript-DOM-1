4a694b8bd19ec77eaf5dc175e3f471c62ba2d0c7
armands62 <armandsliepa3@gmail.com>
2021-03-16 16:03:13 +0200

    Lesson 7

--- 07-automatic-tabbing-fields/global.js
+++ 07-automatic-tabbing-fields/global.js
@@ -1,1 +1,27 @@
 // Your JavaScript goes here.
+window.addEventListener('load', function(){
+    const first = document.getElementById('first');
+    const second = document.getElementById('second');
+    const third = document.getElementById('third');
+
+    first.addEventListener('keyup', function(){
+        typed = first.value.length;
+        max = parseInt(first.getAttribute("maxlength"));
+
+        if (typed === max){
+            second.focus();
+        }
+    });
+
+    second.addEventListener('keyup', function(){
+        typed = second.value.length;
+        max = parseInt(second.getAttribute("maxlength"));
+
+        if (typed === max){
+            third.focus();
+        }
+    });
+    if (typed_characters === max_characters) {
+        end_numbers.focus();
+    }
+});
