456ffa42b463a405ddd95c2f8c76ecd5b139d089
armands62 <armandsliepa3@gmail.com>
2021-03-16 15:34:21 +0200

    Lesson 5

--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -1,1 +1,15 @@
 // Your JavaScript goes here.
+window.addEventListener('load', function(){
+    const Toggle = document.querySelectorAll('.second_five li')
+    const Button = document.getElementById("toggle_button");
+
+    Button.addEventListener('click', function(){
+        Toggle.forEach((e) => {
+            if(e.style.display == 'flex') {
+                e.style.display = 'none'
+            } else {
+                e.style.display = 'flex'
+            }
+        });
+    });
+});
