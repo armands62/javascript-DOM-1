590fd71204accbcbfa95f382d1e2b8c8fa9fb82c
armands62 <armandsliepa3@gmail.com>
2021-03-16 15:23:29 +0200

    Lesosn 4

--- 03-show-many-elements/QUESTIONS.md
+++ 03-show-many-elements/QUESTIONS.md
@@ -6,4 +6,4 @@
 
 > How did you go about hiding elements initially?
 
-Your reply here...
+I changed the style to be "none" at the start.
--- 04-toggle-many-elements/global.js
+++ 04-toggle-many-elements/global.js
@@ -1,1 +1,15 @@
 // Your JavaScript goes here.
+window.addEventListener('load', function(){
+    const Toggle = document.getElementsByClassName('toggle_me');
+    const Button = document.getElementById("button");
+
+    Button.addEventListener('click', function(){
+        Array.from(Toggle).forEach((e) => {
+            if(e.style.display == 'none') {
+                e.style.display = 'flex'
+            }else{
+                e.style.display = 'none'
+            }
+        });
+    });
+});
